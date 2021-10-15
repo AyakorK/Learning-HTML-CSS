abb68aab8ce3eb8d266889dac7c024003f0ebbdf
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2021-10-15 10:54:46 +0200

    Website (not totally responsive)

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,14 @@ L1 = document.getElementsByClassName('line1')[0];
 L2 = document.getElementsByClassName('line2')[0];
 L3 = document.getElementsByClassName('line3')[0];
 L4 = document.getElementsByClassName('line4')[0];
-FIP = document.getElementsByClassName('Switch')[0]
-SIP = document.getElementsByClassName('Switch')[1]
+FIP = document.getElementsByClassName('Switch')[0];
+SIP = document.getElementsByClassName('Switch')[1];
+C1 = document.getElementsByClassName('categories')[0]
+C2 = document.getElementsByClassName('categories')[1]
+C3 = document.getElementsByClassName('categories')[2]
+C4 = document.getElementsByClassName('categories')[3]
+DL = document.getElementById('download')
+
 
 FIP.addEventListener('click', () => {
     if (A1P2.style.visibility == 'visible' && A1P1.style.visibility == 'hidden') {
@@ -42,3 +48,28 @@ SIP.addEventListener('click', () => {
     }
 
 });
+
+C1.addEventListener('mouseover', () => {
+    C1.style.width = '90px';
+})
+C1.addEventListener('mouseout', () => {
+    C1.style.width = '30px';
+})
+C2.addEventListener('mouseover', () => {
+    C2.style.width = '90px';
+})
+C2.addEventListener('mouseout', () => {
+    C2.style.width = '30px';
+})
+C3.addEventListener('mouseover', () => {
+    C3.style.width = '90px';
+})
+C3.addEventListener('mouseout', () => {
+    C3.style.width = '30px';
+})
+C4.addEventListener('mouseover', () => {
+    C4.style.width = '90px';
+})
+C4.addEventListener('mouseout', () => {
+    C4.style.width = '30px';
+})
